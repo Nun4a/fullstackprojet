d2f0f503ef5362895be6e7e09a5bf85e5c2a3505
Iriamu19 <guillaume.guay@pm.me>
2022-10-07 16:19:33 +0200

    Ajout relations

--- api/src/main/java/org/polytech/covidapi/model/Doctor.java
+++ api/src/main/java/org/polytech/covidapi/model/Doctor.java
@@ -1,10 +1,16 @@
 package org.polytech.covidapi.model;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 //@DiscriminatorValue("3")
 public class Doctor extends User{
+
+    @OneToOne
+    @JoinColumn(name = "center_id", referencedColumnName = "id")
+    private Center center;
 
     @Override
     public String toString() {

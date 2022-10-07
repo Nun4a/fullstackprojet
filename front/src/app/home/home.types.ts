d78cbbe15550a39fe48438f0af4ac7dae310018f
BaptisteP <baptiste.porcu@gmail.com>
2022-10-07 14:36:25 +0200

    Merge main

---
+++ front/src/app/home/home.types.ts
@@ -0,0 +1,13 @@
+export type centerType = {
+    id: number;
+    name: string;
+    adress: string
+  }
+
+export type reserveDataToSend = {
+    centerId: number;
+    fname: string;
+    lname: string;
+    mail: string;
+    daterdv: string;
+}

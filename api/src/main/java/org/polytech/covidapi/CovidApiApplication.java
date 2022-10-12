456e1eaed715f35493e08a293daaad6907dbf8ef
Iriamu19 <guillaume.guay@pm.me>
2022-10-12 17:26:50 +0200

    Enlever les requêtes SQL du fichier app

--- api/src/main/java/org/polytech/covidapi/CovidApiApplication.java
+++ api/src/main/java/org/polytech/covidapi/CovidApiApplication.java
@@ -1,61 +1,13 @@
 package org.polytech.covidapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class CovidApiApplication implements CommandLineRunner{
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+public class CovidApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidApiApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
-		String sql = "INSERT INTO address (id, city, street, zipcode) VALUES ('0','Metz', 'Ici', '57000')";
-		int rows = jdbcTemplate.update(sql);
-		if (rows > 0){
-			System.out.println("une colone a été ajouté");
-			
-		}
-
-		sql = "INSERT INTO address (city, street, zipcode) VALUES ('Nancy', 'Place Stanislat', '54000')";
-		rows = jdbcTemplate.update(sql);
-		if (rows > 0){
-			System.out.println("une colone a été ajouté");
-			
-		}
-
-		sql = "INSERT INTO patient (id, first_name, last_name, mail, phone_number, id_address, vaccinated) VALUES ('0','Jean', 'Dupond', 'Jean.Dupond@mail.com','0606060606', '1' , true)";
-		rows = jdbcTemplate.update(sql);
-		if (rows > 0){
-			System.out.println("une colone a été ajouté");
-			
-		}
-
-		sql = "INSERT INTO admin (id, first_name, last_name, mail, phone_number, id_address) VALUES ('0','admin', 'admin', 'admin','1234567', '0' )";
-		rows = jdbcTemplate.update(sql);
-		if (rows > 0){
-			System.out.println("une colone a été ajouté");
-			
-		}
-
-		sql = "INSERT INTO super_admin (id, first_name, last_name, mail, phone_number, id_address) VALUES ('0','supadmin', 'supadmin', 'supadmin','1234567', '0' )";
-		rows = jdbcTemplate.update(sql);
-		if (rows > 0){
-			System.out.println("une colone a été ajouté");
-		}
-	
-	}
-
-
 
 }

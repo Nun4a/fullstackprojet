1f742e6f155427ccd259ddad90d4885948afd8a2
Iriamu19 <guillaume.guay@pm.me>
2022-11-25 18:00:17 +0100

    ajout bucket

--- api/src/main/java/org/polytech/covidapi/controller/AppointmentController.java
+++ api/src/main/java/org/polytech/covidapi/controller/AppointmentController.java
@@ -1,24 +1,29 @@
 package org.polytech.covidapi.controller;
 
+import java.util.Optional;
+
 import org.polytech.covidapi.model.Appointment;
+import org.polytech.covidapi.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Optional;
-
-import org.polytech.covidapi.service.AppointmentService;
 
 @RestController
 @RequestMapping("/api")
 public class AppointmentController {
     @Autowired
     private AppointmentService appointmentService;
+
+    //rajoute 10 tokens toutes les minutes
+    Refill refill = Refill.intervally(10, Duration.ofMinutes(1));
+    //capacité max de 10 token
+    Bandwidth limit = Bandwidth.classic(10, refill);
+    Bucket bucket = Bucket.builder().addLimit(limit).build();
 
     @GetMapping(value="/appointments")
     public Iterable<Appointment> getAllAppointment(){
@@ -33,7 +38,9 @@ public class AppointmentController {
 
     @PostMapping(path = "/appointment")
     public Appointment save(@RequestBody Appointment newappointment) {
-        return appointmentService.save(newappointment);
+        if(bucket.tryConsume(1)) {
+            return appointmentService.save(newappointment);
+            }
     }
 
     @DeleteMapping("/appointment/{id}")

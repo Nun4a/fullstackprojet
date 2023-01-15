0d7a91f3dcd0e6d9deabd4f5447ee28aee45fc90
Iriamu19 <guillaume.guay@pm.me>
2023-01-15 22:49:34 +0100

    changement pour waiting, material progress

--- front/src/app/component/waiting/waiting.component.ts
+++ front/src/app/component/waiting/waiting.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { identifierName } from '@angular/compiler';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { interval } from 'rxjs/internal/observable/interval';
 
 @Component({
   selector: 'app-waiting',
   templateUrl: './waiting.component.html',
   styleUrls: ['./waiting.component.css']
 })
-export class WaitingComponent implements OnInit {
+export class WaitingComponent implements OnInit, OnDestroy  {
 
-  constructor() { }
+  progressbarValue = 100;
+  tempsInitial: number = 0;
+  private sub: any;
+  finishedTimer: boolean = false;
+  tempsRestant: number = 0;
 
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+    ) { }
+  
   ngOnInit(): void {
+    console.log('waiting');
+    this.sub = this.route.params.subscribe(params => {
+      this.tempsInitial = +params['temps'];
+    });
+    if(this.tempsInitial === 0) {
+      this.finishedTimer = true;
+    }
+    this.startTimer();
+  }
+  
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    
+  }
+
+  startTimer() {
+    const tempsInitial = this.tempsInitial;
+    this.tempsRestant = this.tempsInitial;
+    const timer$ = interval(1000);
+
+    const sub = timer$.subscribe((sec) => {
+      sec = sec+1;
+      this.progressbarValue = 100 - sec * 100 / tempsInitial;
+
+      this.tempsRestant = tempsInitial - sec;
+      if (sec === tempsInitial) {
+        sub.unsubscribe();
+        this.finishedTimer = true;
+      }
+    });
+  }
+
+  backToHome() {
+    this.router.navigate(['/home']);
   }
 
 }
